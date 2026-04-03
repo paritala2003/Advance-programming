9f59ea2de70df4938325a8829cda4fe9a662aee7
Paritala Raghavendra <paritala2003@gmail.com>
2026-04-04 00:21:10 +0100

    Renamed folder to remove space

--- Task2_PerodicTable/obj/Debug/net8.0/Task2_PeriodicTable.AssemblyInfo.cs
+++ Task2_PerodicTable/obj/Debug/net8.0/Task2_PeriodicTable.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2_PeriodicTable")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64ca5fd0c0fb476c8d45da405ea8cb5c800c4a67")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2_PeriodicTable")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2_PeriodicTable")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
